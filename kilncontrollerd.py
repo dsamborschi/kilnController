c0047bbd4113f89ac894218ff4c26e7a906a4aae
dsamborschi <dsamborschi@yahoo.com>
2021-03-31 18:16:46 -0400

    Update kilncontrollerd.py

--- kilncontrollerd.py
+++ kilncontrollerd.py
@@ -205,7 +205,7 @@ def save_profile(profile, force=False):
      result=self.collection.insert_one(mydict)
      print (result.inserted_id)
    except Exception:
-        traceback.print_exc()
+    traceback.print_exc()
         
     return True
 
