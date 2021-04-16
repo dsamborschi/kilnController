c1fed24ef232f89e2b39c2951731c5814359e574
dsamborschi <dsamborschi@yahoo.com>
2021-04-16 10:26:02 -0400

    TEST CHANGE

--- lib/oven2.py
+++ lib/oven2.py
@@ -159,11 +159,9 @@ class Oven(threading.Thread):
         if value * 1000 >= millis() - pidstart:
             self.heat = 1.0
             GPIO.output(config.gpio_heat, GPIO.HIGH)
-            log.info("Heat is ON")
         else:
             self.heat = 0.0
             GPIO.output(config.gpio_heat, GPIO.LOW)
-            log.info("Heat is OFF")
 
     def get_state(self):
         state = {
