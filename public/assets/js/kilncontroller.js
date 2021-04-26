fc9e0f95cf2a1c13655a930c54004a3237e7a284
dsamborschi <dsamborschi@yahoo.com>
2021-04-25 21:41:50 -0400

    TEST CHANGE

--- public/assets/js/kilncontroller.js
+++ public/assets/js/kilncontroller.js
@@ -70,7 +70,7 @@ function updateProfile(id)
       for (var i=0; i<profiles[id].data.length; i++)
        {
         //new_data[j+1][0] = new_data[j][0] + profiles[id].data[i][1]/(profiles[id].data[i][0]/60);
-        new_data.push([new_data[j][0] + profiles[id].data[i][1]/(profiles[id].data[i][0]/60),profiles[id].data[i][1]]);
+        new_data.push([new_data[j][0] + (profiles[id].data[i][1]-new_data[j][1])/(profiles[id].data[i][0]/60),profiles[id].data[i][1]]);
         //new_data[j+1][1] = profiles[id].data[i][1];
         //new_data[j+2][0] = new_data[j+1][0] + profiles[id].data[i][2];
         //new_data[j+2][1] = profiles[id].data[i][1];
