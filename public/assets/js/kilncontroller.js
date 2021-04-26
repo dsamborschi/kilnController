0c6c4ad349ec49281ec891d5861e3979d5e4d0b1
dsamborschi <dsamborschi@yahoo.com>
2021-04-25 20:34:47 -0400

    TEST CHANGE

--- public/assets/js/kilncontroller.js
+++ public/assets/js/kilncontroller.js
@@ -64,17 +64,17 @@ function updateProfile(id)
     if (selected_profile_type == "ramp-hold"){
       console.log (profiles[id].data);
       var new_data = [];
-      new_data.push(0,68);
+      new_data.push([0,68]);
       console.log (new_data);
       var j = 0
       for (var i=0; i<profiles[id].data.length; i++)
        {
         //new_data[j+1][0] = new_data[j][0] + profiles[id].data[i][1]/(profiles[id].data[i][0]/60);
-        new_data.push((new_data[j][0] + profiles[id].data[i][1]/(profiles[id].data[i][0]/60)),profiles[id].data[i][1]);
+        new_data.push([(new_data[j][0] + profiles[id].data[i][1]/(profiles[id].data[i][0]/60)),profiles[id].data[i][1]]);
         //new_data[j+1][1] = profiles[id].data[i][1];
         //new_data[j+2][0] = new_data[j+1][0] + profiles[id].data[i][2];
         //new_data[j+2][1] = profiles[id].data[i][1];
-        new_data.push(new_data[j+1][0] + profiles[id].data[i][2],profiles[id].data[i][1]);
+        new_data.push([new_data[j+1][0] + profiles[id].data[i][2],profiles[id].data[i][1]]);
         j = j + 2
        }
 
